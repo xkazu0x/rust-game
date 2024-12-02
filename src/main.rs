43dfdc25b68c628ff862938685fb0bb02b14b90d
xkazu0x <kazuooficial@hotmail.com>
2024-12-02 00:56:12 -0300

    fucking stupid triangle

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use sdl2::{
-    Sdl, video::{Window, GLContext, SwapInterval}, EventPump
-};
+use sdl2::{Sdl, EventPump, video::{Window, GLContext, SwapInterval}};
+use std::{ffi::{CStr, CString}, ptr::{null, null_mut}};
+use gl::types::{GLuint, GLint, GLchar, GLenum};
 
 struct SxWindow {
     sdl:Sdl,
@@ -38,7 +38,7 @@ impl SxWindow {
         
         let event = sdl.event_pump().unwrap();
         
-        Ok(SxWindow{
+        Ok(SxWindow {
             sdl,
             gl_context,
             gl,
@@ -48,9 +48,258 @@ impl SxWindow {
     }
 }
 
-fn main() {
-    let mut window = SxWindow::new("RUST GAME", 960, 540).unwrap();
+fn create_whitespace_cstring_with_len(len:usize) -> CString {
+    let mut buffer:Vec<u8> = Vec::with_capacity(len + 1);
+    buffer.extend([b' '].iter().cycle().take(len));
+    unsafe { CString::from_vec_unchecked(buffer) }
+}
+
+struct Shader {
+    id:GLuint,
+}
+
+impl Shader {
+    fn from_source(source: &CStr, kind: GLenum) -> Result<Self, String> {
+        let id = unsafe {gl::CreateShader(kind)};
+        unsafe {
+            gl::ShaderSource(id, 1, &source.as_ptr(), null());
+            gl::CompileShader(id);
+        }
+
+        let mut success:GLint = 1;
+        unsafe { gl::GetShaderiv(id, gl::COMPILE_STATUS, &mut success); }
+        if success == 0 {
+            let mut len:GLint = 0;
+            unsafe { gl::GetShaderiv(id, gl::INFO_LOG_LENGTH, &mut len); }
+
+            let error = create_whitespace_cstring_with_len(len as usize);
+            unsafe { gl::GetShaderInfoLog(id, len, null_mut(), error.as_ptr() as *mut GLchar); }
+            return Err(error.to_string_lossy().into_owned());
+        }
+        
+        Ok(Shader {id})
+    }
+
+    fn id(&self) -> GLuint {
+        self.id
+    }
+}
+
+impl Drop for Shader {
+    fn drop(&mut self) {
+        unsafe { gl::DeleteShader(self.id); }
+    }
+}
+
+struct Program {
+    id: GLuint,
+}
+
+impl Program {
+    fn from_shaders(shaders: &[Shader]) -> Result<Self, String> {
+        let id = unsafe { gl::CreateProgram() };
+
+        for shader in shaders {
+            unsafe { gl::AttachShader(id, shader.id()); }
+        }
+
+        unsafe { gl::LinkProgram(id); }
+
+        let mut success:GLint = 1;
+        unsafe { gl::GetProgramiv(id, gl::LINK_STATUS, &mut success); }
+        if success == 0 {
+            let mut len:GLint = 0;
+            unsafe { gl::GetProgramiv(id, gl::INFO_LOG_LENGTH, &mut len); }
+
+            let error = create_whitespace_cstring_with_len(len as usize);
+            unsafe { gl::GetProgramInfoLog(id, len, null_mut(), error.as_ptr() as *mut GLchar); }
+            return Err(error.to_string_lossy().into_owned());
+        }
+        
+        Ok(Program {id})
+    }
+
+    fn set(&self) {
+        unsafe { gl::UseProgram(self.id); }
+    }
+}
+
+impl Drop for Program {
+    fn drop(&mut self) {
+        unsafe { gl::DeleteShader(self.id); }
+    }
+}
+
+struct Vbo {
+    id:GLuint,
+}
+
+impl Vbo {
+    fn gen() -> Self {
+        let mut id: GLuint = 0;
+        unsafe { gl::GenBuffers(1, &mut id); }
+        Vbo {id}
+    }
+
+    fn set(&self, data: &Vec<f32>) {
+        self.bind();
+        self.data(data);
+    }
+
+    fn data(&self, data: &Vec<f32>) {
+        unsafe {
+            gl::BufferData(
+                gl::ARRAY_BUFFER,
+                (data.len() * std::mem::size_of::<f32>()) as gl::types::GLsizeiptr,
+                data.as_ptr() as *const gl::types::GLvoid,
+                gl::STATIC_DRAW,
+            );
+        }
+    }
     
+    fn bind(&self) {
+        unsafe { gl::BindBuffer(gl::ARRAY_BUFFER, self.id); }
+    }
+
+    fn unbind(&self) {
+        unsafe { gl::BindBuffer(gl::ARRAY_BUFFER, 0); }
+    }
+
+    fn delete(&self) {
+        unsafe { gl::DeleteBuffers(1, &self.id) }
+    }
+}
+
+impl Drop for Vbo {
+    fn drop(&mut self) {
+        self.unbind();
+        self.delete();
+    }
+}
+
+struct Ebo {
+    id:GLuint,
+}
+
+impl Ebo {
+    fn gen() -> Self {
+        let mut id: GLuint = 0;
+        unsafe { gl::GenBuffers(1, &mut id); }
+        Ebo {id}
+    }
+
+    fn set(&self, data: &Vec<u32>) {
+        self.bind();
+        self.data(data);
+    }
+
+    fn data(&self, indices: &Vec<u32>) {
+        unsafe {
+            gl::BufferData(
+                gl::ELEMENT_ARRAY_BUFFER,
+                (indices.len() * std::mem::size_of::<u32>()) as gl::types::GLsizeiptr,
+                indices.as_ptr() as *const gl::types::GLvoid,
+                gl::STATIC_DRAW,
+            );
+        }
+    }
+    
+    fn bind(&self) {
+        unsafe { gl::BindBuffer(gl::ELEMENT_ARRAY_BUFFER, self.id); }
+    }
+
+    fn unbind(&self) {
+        unsafe { gl::BindBuffer(gl::ELEMENT_ARRAY_BUFFER, 0); }
+    }
+
+    fn delete(&self) {
+        unsafe { gl::DeleteBuffers(1, &self.id) }
+    }
+}
+
+impl Drop for Ebo {
+    fn drop(&mut self) {
+        self.unbind();
+        self.delete();
+    }
+}
+
+struct Vao {
+    id:GLuint,
+}
+
+impl Vao {
+    fn gen() -> Self {
+        let mut id: GLuint = 0;
+        unsafe { gl::GenVertexArrays(1, &mut id); }
+        Vao {id}
+    }
+
+    fn set(&self) {
+        self.bind();
+        self.setup();
+    }    
+
+    fn setup(&self) {
+        unsafe {
+            gl::EnableVertexAttribArray(0);
+            gl::VertexAttribPointer(
+                0,
+                2,
+                gl::FLOAT,
+                gl::FALSE,
+                (2 * std::mem::size_of::<f32>()) as GLint,
+                null());
+        }
+    }
+    
+    fn bind(&self) {
+        unsafe { gl::BindVertexArray(self.id); }
+    }
+
+    fn unbind(&self) {
+        unsafe { gl::BindVertexArray(0); }        
+    }
+
+    fn delete(&self) {
+        unsafe { gl::DeleteVertexArrays(1, &self.id) }
+    }
+}
+
+impl Drop for Vao {
+    fn drop(&mut self) {
+        self.unbind();
+        self.delete();
+    }
+}
+
+fn main() -> Result<(), String> {
+    let mut window = SxWindow::new("RUST GAME", 960, 540).unwrap();
+
+    let vertex_shader = Shader::from_source(&CString::new(include_str!("shader.vert")).unwrap(), gl::VERTEX_SHADER).unwrap();
+    let fragment_shader = Shader::from_source(&CString::new(include_str!("shader.frag")).unwrap(), gl::FRAGMENT_SHADER).unwrap();
+    let shader_program = Program::from_shaders(&[vertex_shader, fragment_shader]).unwrap();
+    shader_program.set();
+
+    let vertices:Vec<f32> = vec![
+         0.0,  0.5,
+         0.5, -0.5,
+        -0.5, -0.5,
+    ];
+
+    let indices:Vec<u32> = vec![
+        0, 1, 2,
+    ];
+    
+    let vbo = Vbo::gen();
+    vbo.set(&vertices);
+
+    let vao = Vao::gen();
+    vao.set();
+    
+    let ebo = Ebo::gen();
+    ebo.set(&indices);
+        
     'running: loop {
         for event in window.event.poll_iter() {
             match event {
@@ -65,9 +314,17 @@ fn main() {
             unsafe {
                 gl::ClearColor(0.25, 0.3, 0.3, 1.0);
                 gl::Clear(gl::COLOR_BUFFER_BIT);
+
+                gl::DrawElements(
+                    gl::TRIANGLES,
+                    indices.len() as i32,
+                    gl::UNSIGNED_INT,
+                    0 as *const _);
             }
 
             window.handle.gl_swap_window();
         }
     }
+
+    Ok(())
 }
---
+++ src/shader.frag
@@ -0,0 +1,7 @@
+#version 330 core
+
+out vec4 out_color;
+
+void main() {
+    out_color = vec4(0.4, 0.35, 0.37, 1.0);
+}
